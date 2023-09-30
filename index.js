9ee04469ed38b2585fa26c95d34faa48756fa143
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-30 10:25:12 +0200

    add function CreateAccount

--- index.js
+++ index.js
@@ -27,3 +27,16 @@ const strongPassword = (len) => {
 
   return result;
 };
+
+const createAccount = (user, generatePassword) => {
+  if (!user || typeof generatePassword !== 'function') {
+    return null;
+  }
+  const password = generatePassword();
+  return `Uživatel ${user} s heslem ${generatePassword(9)}`;
+};
+document.body.innerHTML += `
+	<p>${createAccount('Míša', weakPassword)}</p>
+	<p>${createAccount('Řízek', mediumPassword)}</p>
+	<p>${createAccount('Mápodčepicí', strongPassword)}</p>
+`;
